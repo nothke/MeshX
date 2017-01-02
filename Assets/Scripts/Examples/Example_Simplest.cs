4a8c04531b445d0c3623cdd2bb82e15af4fc4b97
Ivan Notaros <nothke@gmail.com>
2017-01-02 13:42:40 +0100

    Added simplest one-line example

---
+++ Assets/Scripts/Examples/Example_Simplest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MeshXtensions;
+
+public class Example_Simplest : MonoBehaviour
+{
+    public Material material;
+
+    void Start()
+    {
+        gameObject.InitializeMesh(MeshX.Cube(), material);
+    }
+}
