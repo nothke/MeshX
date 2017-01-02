fff7b34e9385c07137014fa8f6ad26955d170dd6
Ivan Notaros <nothke@gmail.com>
2017-01-02 21:53:19 +0100

    Added simplex noise from Catlike Coding

--- Assets/Scripts/Examples/Example_QuadSphere.cs
+++ Assets/Scripts/Examples/Example_QuadSphere.cs
@@ -11,13 +11,33 @@ public class Example_QuadSphere : MonoBehaviour
     {
         Geom g = MeshX.GeomQuadSphere(100, 10);
 
+        NoiseMethod method = Noise.simplexMethods[2];
+
+        float mult = 0.05f;
+        float thres = -0.1f;
+
         for (int i = 0; i < g.vertices.Count; i++)
         {
-            g.vertices[i] *= 1 + Random.value * 0.02f;
+            //NoiseSample sample = Noise.Simplex3D(g.vertices[i], 0.6f);
+
+            NoiseSample sample = Noise.Sum(method, g.vertices[i], 0.1f, 4, 2, 0.5f);
+
+            Vector3 normal = g.vertices[i].normalized;
+
+            if (sample.value < thres)
+            {
+                g.vertices[i] *= 1 + mult * thres;
+                g.normals[i] = normal;
+            }
+            else
+            {
+                g.vertices[i] *= 1 + sample.value * mult;
+                g.normals[i] = (normal - sample.derivative * 1).normalized;
+            }
         }
 
         Mesh m = g.ToMesh();
-        m.RecalculateNormals();
+        //m.RecalculateNormals();
 
         gameObject.InitMesh(m);
     }
