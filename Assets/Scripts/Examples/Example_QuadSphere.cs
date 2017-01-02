d9d624b0f8cc855cb14300ec1ff054be6d83a756
Ivan Notaros <nothke@gmail.com>
2017-01-02 20:39:41 +0100

    A simple QuadSphere example

---
+++ Assets/Scripts/Examples/Example_QuadSphere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MeshXtensions;
+
+public class Example_QuadSphere : MonoBehaviour
+{
+
+    void Start()
+    {
+        Geom g = MeshX.GeomQuadSphere(100, 10);
+
+        for (int i = 0; i < g.vertices.Count; i++)
+        {
+            g.vertices[i] *= 1 + Random.value * 0.02f;
+        }
+
+        Mesh m = g.ToMesh();
+        m.RecalculateNormals();
+
+        gameObject.InitMesh(m);
+    }
+}
