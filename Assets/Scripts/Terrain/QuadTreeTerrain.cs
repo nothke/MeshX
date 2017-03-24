3d10c231e3e5883d22811ee518a8fe33ded193ea
Ivan Notaros <nothke@gmail.com>
2017-03-25 00:01:13 +0100

    Quad tree terrain script

---
+++ Assets/Scripts/Terrain/QuadTreeTerrain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadTreeTerrain : MonoBehaviour
+{
+
+    MyOwnQuadTree quadTree;
+
+    void Start()
+    {
+        quadTree = GetComponent<MyOwnQuadTree>();
+
+        StartCoroutine(WaitFrame());
+    }
+
+    IEnumerator WaitFrame()
+    {
+        yield return null;
+
+        List<TreeNode> treeNodes = quadTree.tree.root.GetAllChildren();
+
+        foreach (var treeNode in treeNodes)
+        {
+            if (treeNode.IsSplit) continue;
+
+            GameObject go = new GameObject();
+
+            Vector3 pos = treeNode.bounds.min;
+            pos.z = pos.y;
+            pos.y = 0;
+
+            go.transform.position = pos;
+
+            TerrainTile tile = go.AddComponent<TerrainTile>();
+            tile.offset = treeNode.bounds.min;
+            tile.width = quadTree.size / Mathf.Pow(2, treeNode.depth);
+        }
+    }
+}
