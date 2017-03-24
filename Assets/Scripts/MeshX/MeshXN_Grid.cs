3b11a73ca4a78fa47bc8eca5aff85edefb0b13b6
Ivan Notaros <nothke@gmail.com>
2017-03-24 21:18:12 +0100

    Added Grid

---
+++ Assets/Scripts/MeshX/MeshXN_Grid.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MeshXtensions
+{
+    public class Grid : Surf
+    {
+        public int width = 2;
+
+        public int length { get { return (vertices.Length / width); } }
+
+        public Grid(int width, params Vector3[] points) : base(points)
+        {
+            this.width = width;
+        }
+
+        public Grid(int width, params float[] pointCoords) : base(pointCoords)
+        {
+            this.width = width;
+        }
+
+        public static Grid Create(int width, int length, float separation)
+        {
+            Vector3[] points = new Vector3[width * length];
+
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    points[y * width + x] = new Vector3(x * separation, 0, y * separation);
+                }
+            }
+
+            return new Grid(width, points);
+        }
+
+        public void AddHeights(float[] heights)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    vertices[y * width + x] += Vector3.up * heights[y * width + x];
+                }
+            }
+        }
+
+        public void AddVectors(Vector3[] v3s)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    vertices[y * width + x] += v3s[y * width + x];
+                }
+            }
+        }
+
+        public void AddVector(Vector3 v3)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    vertices[y * width + x] += v3;
+                }
+            }
+        }
+
+        public override List<int> GetTris(int startIndex = 0)
+        {
+            if (width < 2) width = 2;
+
+            int vertLength = vertices.Length;
+
+            if (vertLength < 3) return null;
+
+            List<int> indices = new List<int>();
+
+            // 0, width, 1
+            // 1, width, width + 1
+
+            // 2, width + 1, 3
+            // 3, width + 1, width + 2
+
+            // i, i + width + 1, i + 1
+            // i + 1, i * width + 1, i + width + 2;
+
+            for (int i = 0; i < vertLength - 1; i++)
+            {
+                //int y = i + width;
+
+                if (i % width == width - 1) continue;
+
+                if (startIndex + i + width + 1 >= vertLength)
+                    return indices;
+
+                // tri 1
+                indices.Add(startIndex + i);
+                indices.Add(startIndex + i + width);
+                indices.Add(startIndex + i + 1);
+
+                // tri 2
+                indices.Add(startIndex + i + 1);
+                indices.Add(startIndex + i + width);
+                indices.Add(startIndex + i + width + 1);
+
+            }
+
+            return indices;
+        }
+    }
+}
---
+++ Assets/Scripts/MeshX/MeshX_GridModifiers.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeshXtensions
+{
+
+    public static class MeshXN_Grid
+    {
+        // HEIGHT MODS:
+
+        public static float[] MOD_SmoothSide(this Grid grid, float[] heights, int side = 0, int smoothWidth = 10, float targetHeight = 0)
+        {
+            if (smoothWidth == 0) return heights;
+
+
+            for (int y = 0; y < grid.length; y++)
+            {
+                for (int x = 0; x < grid.width; x++)
+                {
+                    float curHeight = heights[y * grid.width + x];
+
+                    float mult = 1;
+
+                    if (side == 0)
+                        mult = Mathf.Clamp01((float)x / smoothWidth);
+                    else
+                        mult = Mathf.Clamp01((float)(grid.width - 1 - x) / smoothWidth);
+
+
+                    heights[y * grid.width + x] = Mathf.Lerp(targetHeight, curHeight, mult);
+                }
+            }
+
+            return heights;
+        }
+
+        public static float[] MOD_SpatialRidgedGrid(this Grid grid,
+            float frequency, float scale,
+            float xSeparation = 1, float ySeparation = 1,
+            float xOffset = 0, float yOffset = 0, float absPlane = 0.5f)
+        {
+            float[] heights = new float[grid.width * grid.length];
+
+            if (scale == 0 || frequency == 0) return heights;
+
+            for (int y = 0; y < grid.length; y++)
+                for (int x = 0; x < grid.width; x++)
+                {
+                    float value =
+
+                        Mathf.PerlinNoise(
+                            xOffset / xSeparation + x * frequency * xSeparation,
+                            yOffset / ySeparation + y * frequency * ySeparation)
+                            ;
+
+                    if (value > absPlane)
+                        value -= (value - absPlane);
+
+                    heights[y * grid.width + x] = value * scale;
+                }
+
+            return heights;
+        }
+
+        public static float[] MOD_SpatialPerlinGrid(this Grid grid,
+            float frequency, float scale,
+            float xSeparation = 1, float ySeparation = 1,
+            float xOffset = 0, float yOffset = 0)
+        {
+            float[] heights = new float[grid.width * grid.length];
+
+            if (scale == 0 || frequency == 0) return heights;
+
+            for (int y = 0; y < grid.length; y++)
+                for (int x = 0; x < grid.width; x++)
+                    heights[y * grid.width + x] =
+
+                        Mathf.PerlinNoise(
+                            xOffset / xSeparation + x * frequency * xSeparation,
+                            yOffset / ySeparation + y * frequency * ySeparation)
+                            * scale;
+
+            return heights;
+        }
+
+        public static float[] MOD_PerlinGrid(this Grid grid, float frequency, float scale, float xOffset = 0, float yOffset = 0)
+        {
+            float[] heights = new float[grid.width * grid.length];
+
+            for (int y = 0; y < grid.length; y++)
+            {
+                for (int x = 0; x < grid.width; x++)
+                {
+                    heights[y * grid.width + x] = Mathf.PerlinNoise(xOffset + x * frequency, yOffset + y * frequency) * scale;
+                }
+            }
+
+            return heights;
+        }
+
+        public static float[] MOD_CanyonPerlinGrid(this Grid grid, float frequency, float scale)
+        {
+            float[] heights = new float[grid.width * grid.length];
+
+            for (int y = 0; y < grid.length; y++)
+            {
+                for (int x = 0; x < grid.width; x++)
+                {
+                    float value = Mathf.PerlinNoise(x * frequency, y * frequency);
+
+                    if (value > 0.5f) value *= -1;
+
+                    heights[y * grid.width + x] = value * scale;
+                }
+            }
+
+            return heights;
+        }
+
+        public static float[] MOD_RidgedPerlinGrid(this Grid grid, float frequency, float scale)
+        {
+            float[] heights = new float[grid.width * grid.length];
+
+            Debug.Log(grid.length + " " + grid.width);
+            Debug.Log(heights.Length);
+            Debug.Log(grid.vertices.Length);
+
+            for (int y = 0; y < grid.length; y++)
+            {
+                for (int x = 0; x < grid.width; x++)
+                {
+                    float value = Mathf.PerlinNoise(x * frequency, y * frequency);
+
+                    if (value > 0.5f) value = -value + 1;
+
+                    heights[y * grid.width + x] = value * scale * 2 - 50;
+                }
+            }
+
+            return heights;
+        }
+    }
+}
