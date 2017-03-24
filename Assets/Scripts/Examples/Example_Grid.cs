cd5c1ffbbc20d1abdc24d75bbd322d86483093eb
Ivan Notaros <nothke@gmail.com>
2017-03-24 21:59:07 +0100

    Grid example

---
+++ Assets/Scripts/Examples/Example_Grid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MeshXtensions;
+
+public class Example_Grid : MonoBehaviour
+{
+    public int width = 100;
+    public float separation = 1;
+
+
+    void Start()
+    {
+
+        Grid g = Grid.Create(width, width, separation);
+
+        float[] f = g.MOD_RidgedPerlinGrid(0.01f, 20);
+        float[] f2 = g.MOD_RidgedPerlinGrid(0.08f, 3);
+        float[] f3 = g.MOD_CanyonPerlinGrid(0.1f, 3);
+
+        g = g + f + f2 + f3 + 50;
+
+        Mesh m = MeshXN.Combine();
+
+        gameObject.InitMesh(MeshXN.Combine(g));
+    }
+}
